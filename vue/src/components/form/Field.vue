3c631cc9567231f9ce2ceb49556537c8995f8cae
Oziel Perez <OZLperez11@gmail.com>
2019-02-13 00:10:14 -0600

    Added global Field component for forms

---
+++ vue/src/components/form/Field.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="field">
+		<label :for="name" class="label">{{ placeholder }}</label>
+		<div class="control" v-if="type !== 'textarea'">
+			<input :id="name" :type="type" class="input" :placeholder="placeholder" :value="value" @input="updateValue"/>
+		</div>
+		<div class="control" v-else-if="type === 'textarea'">
+			<textarea :id="name" :placeholder="placeholder" :value="value" @input="updateValue"></textarea>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+
+@Component
+export default class Field extends Vue {
+	@Prop() public name!:string;
+	@Prop({default:""}) public value!:string;
+	@Prop({default:"text"}) public type!:string;
+	@Prop({default:""}) public placeholder!:string;
+
+	public updateValue(event:any):void {
+		this.$emit('input', event.target.value); 
+	}
+}
+</script>
+
+<style scoped lang="scss"></style>
