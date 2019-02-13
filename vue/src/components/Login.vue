0540c2dad5d3770ef5be72b7f45e4904a95bca77
Oziel Perez <OZLperez11@gmail.com>
2019-02-13 00:09:35 -0600

    Created Login component

---
+++ vue/src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+	<form autocomplete="off" class="column is-half">
+		<h1>Electro</h1>
+		<field name="login-username" type="text" placeholder="Username" v-model="credentials.username"/>
+		<field name="login-password" type="password" placeholder="Password" v-model="credentials.password"/>
+		<div class="field">
+			<div class="control"><button type="submit" class="button">Log In</button></div>
+		</div>
+	</form>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Emit } from "vue-property-decorator";
+import Field from "./form/Field.vue";
+
+@Component({
+	components:{field:Field}
+})
+export default class Login extends Vue {
+  	public credentials = {
+    	username: null,
+    	password: null
+	};
+
+	@Emit()
+	public submitForm():void {
+		this.$emit("login", this.credentials);
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+	form {
+		max-width:500px;
+		height:305px;
+	}
+</style>
