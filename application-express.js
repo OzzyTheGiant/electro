6776c681e0f57393e3a45d021d30f0a01ff36f44
Oziel Perez <OZLperez11@gmail.com>
2019-03-22 17:13:48 -0500

    Switched from console.log to Bristol logger

--- application-express.js
+++ application-express.js
@@ -2,6 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const logger = require("./express/services/logger");
 const router = require("./express/routes");
 const { sessionConfig } = require("./express/config/session-config");
 const { deleteSessionCookie } = require("./express/middleware/middleware");
@@ -9,6 +10,15 @@ const { NotFoundError } = require("./express/exceptions/exceptions");
 
 const app = express();
 
+/* Log server shut down events */
+process.on('SIGINT', () => logger.notice('Server is shutting down manually'));
+process.on('exit', code => logger.notice(`Server is shutting down with code ${code}`));
+process.on('uncaughtException', error => {
+	logger.error(error);
+	process.exit(1);
+});
+
+/* Middlewares and Router */
 if (process.env.APP_ENV !== "local") app.disable('x-powered-by');
 app.use(bodyParser.json()); // decode json as javascript object
 app.use(cookieParser());
@@ -19,10 +29,12 @@ app.use((request, respose, next) => {
 	next(new NotFoundError("API route")); // Error 404 - Not Found handler
 });
 app.use((error, request, response, next) => { // global error handler
-	console.error(error); // TODO: change log system based on environment
+	logger.error(error);
 	response.status(error.code || 500).json({message:error.message});
 });
 
-app.listen(80, function() { 
-	console.log("Server listening on port 80");
+/* Start server */
+app.listen(process.env.APP_PORT, function() { 
+	logger.notice("Server listening on port " + process.env.APP_PORT);
+	logger.error(new Error("Testing standard log"));
 });
--- express/database/mysql.js
+++ express/database/mysql.js
@@ -1,10 +1,14 @@
 const knex = require("knex");
 const dbConfig = require("../config/db-config");
+const logger = require("../services/logger");
 
 module.exports = knex({ // the knex function will output a connection pool;
 	...dbConfig,
 	afterCreate:(connection, done) => {
-		console.log("connection created");
+		logger.notice("Database connection created", {
+			client:dbConfig.client, 
+			host:dbConfig.connection.host
+		});
 		connection.query('SELECT set_limit(0.01);', error => done(error, connection));
 	}
 });
