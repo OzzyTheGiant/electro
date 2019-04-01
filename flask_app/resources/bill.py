603275bb0ca3a2ae49bd27ee59bfcf20448cca68
Oziel Perez <OZLperez11@gmail.com>
2019-04-01 14:28:44 -0500

    Created bill resource to convert models to JSON

--- flask_app/resources/bill.py
+++ flask_app/resources/bill.py
@@ -1,5 +1,15 @@
-from flask_restful import Resource;
+from flask_restful import Resource, fields, marshal;
+from flask_restful.reqparse import RequestParser;
+from ..models import Bill;
 
-class Bill(Resource):
+class BillResource(Resource):
+	data_fields = {
+		"ID":fields.Integer,
+		"User":fields.Integer,
+		"PaymentAmount":fields.Float,
+		"PaymentDate":fields.String
+	}
+
 	def get(self):
-		return {"message": "Hello world"};
+		bills = Bill.select().dicts();
+		return [marshal(bill, self.data_fields) for bill in bills];
