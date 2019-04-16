ad182545fe2f38efdc4545a62cc15e71557ebb6e
Oziel Perez <OZLperez11@gmail.com>
2019-04-16 11:18:21 -0500

    Reconfigured user model to work with bill model and auth module

--- django_app/application/models.py
+++ django_app/application/models.py
@@ -1,20 +1,25 @@
 """App models"""
-from django.contrib.auth.models import AbstractUser;
+from django.contrib.auth.models import AbstractBaseUser;
 from django.db import models;
 
 # will not be using django's auth User model due to field requirements
-class User(AbstractUser):
+class User(AbstractBaseUser):
 	"""Model for database table Users"""
 	ID = models.IntegerField(primary_key = True);
-	username = models.CharField(max_length = 30, db_column = "Username", unique = True);
-	password = models.CharField(max_length = 255, db_column = "Password");
+	Username = models.CharField(max_length = 30, unique = True);
+	Password = models.CharField(max_length = 255);
+
+	USERNAME_FIELD = 'Username';
+	last_login = None; # this is a default db column from AbstractBaseUser that is unnecessary
 
 	class Meta:
 		db_table = "Users";
+		verbose_name = "user";
+		verbose_name_plural = "users";
 
 class Bill(models.Model):
 	"""Model for database table Bills"""
-	ID = models.IntegerField(primary_key = True);
+	ID = models.AutoField(primary_key = True);
 	User = models.ForeignKey(User, on_delete = models.PROTECT, db_column = "User");
 	PaymentAmount = models.DecimalField(max_digits = 5, decimal_places = 2);
 	PaymentDate = models.DateField();
