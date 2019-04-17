9703b5da8d26a771f41a82303605cc50f774cae2
Oziel Perez <OZLperez11@gmail.com>
2019-04-17 15:55:26 -0500

    Refactored error handler to create validation error messages

--- django_app/application/exceptions.py
+++ django_app/application/exceptions.py
@@ -1,24 +1,36 @@
 """App exception classes and custom exception handler"""
 from rest_framework.views import exception_handler;
 from rest_framework.response import Response;
-from rest_framework.exceptions import APIException;
+from rest_framework.exceptions import APIException, ValidationError;
 
 def global_exception_handler(exception, context):
 	"""Custom global exception handler that will output HTTP and Server errors"""
 	response = exception_handler(exception, context);
-	if response is not None:
-		if isinstance(exception, APIException):
-			response.data["message"] = exception.detail;
-		else:
-			response.data["message"] = exception.args[0];
-		del response.data["detail"]; # duplicate of message, so it is discarded
-	else:
-		# for all other uncaught exceptions
+
+	if response is None:
 		response = Response();
 		response.data = {};
-		response.status_code = 500
-		if isinstance(exception.args[0], int): # since we can't provide custom Database error class, check for sql error code
-			response.data["message"] = "Something went wrong while querying the database";
-		else:
-			response.data["message"] = exception.args[0];
+		response.status_code = 500;
+
+	if isinstance(exception, ValidationError):
+		response.data["message"] = _craft_validation_error_message(exception);
+	elif isinstance(exception, APIException):
+		response.data["message"] = exception.detail;
+	elif isinstance(exception.args[0], int):
+		response.data["message"] = "Something went wrong while querying the database";
+	else:
+		response.data["message"] = exception.args[0];
+
+	if "detail" in response.data:
+		del response.data["detail"]  # duplicate of message, so it is discarded
+
 	return response;
+
+def _craft_validation_error_message(exception):
+	"""iterate over validation error messages to output a sentence or paragraph"""
+	message = "";
+	for field_name, field_errors in exception.detail.items():
+		message += field_name + ": ";
+		for error_detail in field_errors:
+			message += str(error_detail) + " ";
+	return message;
