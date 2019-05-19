ace138f176d21e5b2eccc23bad11c36f4f12880f
Oziel Perez <OZLperez11@gmail.com>
2019-05-19 14:35:05 -0500

    Added custom global exception handler

--- spark/src/electro/App.java
+++ spark/src/electro/App.java
@@ -7,13 +7,17 @@ import electro.controllers.BillController;
 import electro.database.DatabaseAccessor;
 import electro.models.Bill;
 import electro.serializers.BillDeserializer;
+import electro.services.ValidationService;
+import electro.exceptions.HttpException;
 import io.github.cdimascio.dotenv.Dotenv;
+import spark.ExceptionHandler;
 
 public class App {
 	// App dependencies
 	public static final Dotenv env = Dotenv.load();
 	public static final DatabaseAccessor db = new DatabaseAccessor();
 	public static final Gson gson = createGson();
+	public static final ValidationService validator = new ValidationService();
 
     public static void main(String[] args) {
 		port(4567);
@@ -27,8 +31,26 @@ public class App {
 		put(Routes.BILLS_URL + "/:id", BillController.updateBill);
 		delete(Routes.BILLS_URL + "/:id", BillController.deleteBill);
 
+		// notFound((request, response) -> {
+
+		// });
+		exception(Exception.class, exceptionHandler);
+
 		Runtime.getRuntime().addShutdownHook(shutdownEventHandler);
 	}
+
+	public static ExceptionHandler<Exception> exceptionHandler = (exception, request, response) -> {
+		var statusCode = 500;
+		var errorMessage = exception.getMessage();
+		exception.printStackTrace();
+		if (exception instanceof HttpException) {
+			HttpException httpException = (HttpException) exception;
+			statusCode = httpException.getStatusCode();
+			errorMessage = httpException.getMessage();
+		}
+		response.status(statusCode);
+		response.body(String.format("{\"message\": \"%1$s\"}", errorMessage));
+	};
 
 	public static <T> Gson createGson() {
 		var gsonBuilder = new GsonBuilder();
