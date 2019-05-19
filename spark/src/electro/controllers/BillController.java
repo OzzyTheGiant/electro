f139f9ca8199343299f3d678aec483f2e613c47e
Oziel Perez <OZLperez11@gmail.com>
2019-05-19 14:35:58 -0500

    Added a null check to ensure id is set in bill

--- spark/src/electro/controllers/BillController.java
+++ spark/src/electro/controllers/BillController.java
@@ -3,7 +3,10 @@ package electro.controllers;
 import electro.models.Bill;
 import electro.services.BillDatabaseService;
 import spark.Route;
+import java.time.format.DateTimeParseException;
+import electro.exceptions.ValidationException;
 import static electro.App.gson;
+import static electro.App.validator;
 
 public class BillController {
 	public static Route getAllBills = (request, response) -> {
@@ -12,15 +15,17 @@ public class BillController {
 	};
 
 	public static Route addNewBill = (request, response) -> {
-		var bill = gson.fromJson(request.body(), Bill.class);
+		var bill = processJSON(request.body());
 		bill = BillDatabaseService.insertNewBill(bill);
 		response.status(201);
 		return gson.toJson(bill);
 	};
 
 	public static Route updateBill = (request, response) -> {
-		var bill = gson.fromJson(request.body(), Bill.class);
-		BillDatabaseService.updateBill(Integer.parseInt(request.params(":id")), bill);
+		var bill = processJSON(request.body());
+		if (bill.getId() == 0) {
+			bill.setId(Integer.parseInt(request.params(":id")));
+		} BillDatabaseService.updateBill(bill);
 		return request.body();
 	};
 
@@ -29,4 +34,17 @@ public class BillController {
 		response.status(204);
 		return "";
 	};
+
+	private static Bill processJSON(String json) throws ValidationException {
+		try {
+			var bill = gson.fromJson(json, Bill.class);
+			var message = validator.validate(bill);
+			if (message != "") {
+				throw new ValidationException(message);
+			}
+			return bill;
+		} catch (DateTimeParseException e) {
+			throw new ValidationException("Payment Date is an invalid date");
+		}
+	}
 }
