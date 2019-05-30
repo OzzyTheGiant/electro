4c5b21db181d16189618419baa12ea6054331a43
Oziel Perez <OZLperez11@gmail.com>
2019-05-30 09:02:28 -0500

    Changed json datetime output format

--- asp.net_core/Startup.cs
+++ asp.net_core/Startup.cs
@@ -43,9 +43,10 @@ namespace electro {
 					.AddThrowOnError(false)
 					.AddEncoding(Encoding.UTF8);
 			});
-			services.AddDbContext<BillContext>(options =>
-				options.UseMySql(connectionString));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+			services.AddDbContext<BillContext>(options => options.UseMySql(connectionString));
+            services.AddMvc()
+				.SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+				.AddJsonOptions(options => options.SerializerSettings.DateFormatString = "yyyy-MM-dd");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
