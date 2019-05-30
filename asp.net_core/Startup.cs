1699812a25b0ab15ad93da59757f574beb29ef16
Oziel Perez <OZLperez11@gmail.com>
2019-05-30 00:45:30 -0500

    Added dotenv package to get db connection settings

--- asp.net_core/Startup.cs
+++ asp.net_core/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,11 +13,20 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using electro.Database;
+using dotenv.net.DependencyInjection.Extensions;
 
-namespace electro
-{
-    public class Startup
-    {
+namespace electro {
+    public class Startup {
+		public string connectionString {
+			get {
+				return 
+					"server=" + Environment.GetEnvironmentVariable("DB_HOST") +
+					";port=" + Environment.GetEnvironmentVariable("DB_PORT") +
+					";database=" + Environment.GetEnvironmentVariable("DB_DATABASE") +
+					";uid=" + Environment.GetEnvironmentVariable("DB_USERNAME") +
+					";password=" + Environment.GetEnvironmentVariable("DB_PASSWORD");
+			}
+		}
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,8 +37,14 @@ namespace electro
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddEnv(builder => {
+				builder
+					.AddEnvFile("../.env")
+					.AddThrowOnError(false)
+					.AddEncoding(Encoding.UTF8);
+			});
 			services.AddDbContext<BillContext>(options =>
-				options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+				options.UseMySql(connectionString));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
