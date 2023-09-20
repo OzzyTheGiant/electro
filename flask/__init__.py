abb5c3e25da0940f99c32b3baece2a7a1bd8b5d8
Oziel Perez <OZLperez11@gmail.com>
2023-09-19 22:48:11 -0500

    Flask: Delete unnecessary module code interfering with flask run command

--- flask/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-if (os.getenv("APP_ENV") is not None):  # to avoid loading these modules during unit testing
-    from models import Bill
-    from config.api import ElectroAPI
-    from config import config
-
-    Bill = Bill
-    ElectroAPI = ElectroAPI
-    config = config
